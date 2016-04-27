91ad3b2011f713dd2494c54e6846c8c5181047f8
Vladislav Grubov <gru212@tproger.ru>
2016-04-27 10:47:44 +0300

    Initial Commit

---
+++ lib/MyWeb.pm
@@ -0,0 +1,38 @@
+package MyWeb;
+use Dancer2;
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+get '/hello' => sub {
+	template 'hello.tt', 
+	{
+		'value' => 4156234,
+	};
+};
+
+get '/reg' => sub {
+	template 'registration.tt',
+	{
+		'csrf_value' => 123123,
+	};
+};
+
+post '/reg' => sub {
+	my (@args) = @_;
+	
+	use Data::Dumper;
+	use FindBin;
+
+	open (my $dh, '>', $FindBin::Bin . '/../logs.txt');
+	$dh->print(Dumper(\@args));
+	close($dh);
+
+	my $some_var = 123;
+	template 'ok'
+};
+
+true;
