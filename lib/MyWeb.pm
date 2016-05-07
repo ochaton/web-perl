0bfc386ab9dbcb42e7d9b41ff13bc8b3cadeacea
Vladislav Grubov <gru212@tproger.ru>
2016-05-07 13:27:32 +0300

    Borderline commit

--- lib/MyWeb.pm
+++ lib/MyWeb.pm
@@ -3,6 +3,9 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+use DBI;
+my $dbh = DBI->connect('dbi:mysql:database=Sfera;' . 'host=localhost;port=3306', 'root', 'imagination');
+
 get '/' => sub {
     template 'index';
 };
@@ -28,6 +31,17 @@ get '/auth' => sub {
 	};
 };
 
+get '/user' => sub {
+
+	template 'user_page.tt',
+	{
+		'user_img' => 'img_src',
+		'user_nick' => 'ochaton',
+		'user_email' => 'ochaton',
+		'user_token' => 'ochaton',
+	}
+};
+
 post '/auth' => sub {
 	my (@args) = @_;
 
@@ -38,9 +52,7 @@ post '/auth' => sub {
 	my $password = params->{'password'};
 	my $sig = params->{'sig'};
 
-	use DBI;
-	my $dbh = DBI->connect('dbi:mysql:database=Sfera;' . 'host=localhost;port=3306', 'root', 'imagination');
-	my $sql = 'SELECT * FROM `users` WHERE email = ?';
+	my $sql = 'SELECT * FROM `login` WHERE email = ?';
     my $sth = $dbh->prepare($sql) or die $dbh->errstr;
     
     $sth->execute(params->{'email'}) or die $sth->errstr;
@@ -69,7 +81,66 @@ post '/auth' => sub {
 };
 
 post '/reg' => sub {
-	
-}
+	my (@args) = @_;
+
+	if (params->{password} ne params->{passcheck}) {
+		template 'ok',
+		{
+			'msg' => 'WA!<br />Пароли не совпадают!',
+		};
+		return ;
+	}
+
+	my $sql = 'SELECT * FROM `login` WHERE email=?';
+	my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+    
+    $sth->execute(params->{'email'}) or die $sth->errstr;
+
+    my $row = $sth->fetchrow_hashref;
+    p $row;
+
+    if (defined $row) {
+    	template 'ok',
+    	{
+    		'msg' => 'WA!<br />Пользователь с таким email уже существует!',
+    	};
+    	return ;
+    }
+
+	my $user_info = {
+		nick => params->{'nick'},
+		email => params->{'email'},
+		password => params->{'password'},
+	};
+
+	$sql = 'INSERT INTO `users` (nick, email, token) VALUES (?, ?, ?)';
+	$sth = $dbh->prepare($sql) or die $dbh->errstr;
+	$sth->execute($user_info->{nick}, $user_info->{email}, $user_info->{nick} . $user_info->{email}) or 
+		die $sth->errstr;
+
+	$sql = 'SELECT id FROM `users` WHERE nick = ?';
+	$sth = $dbh->prepare($sql) or die $dbh->errstr;
+	$sth->execute($user_info->{nick}) or die $dbh->errstr;
+
+    $row = $sth->fetchrow_hashref;
+    p $row;
+
+    unless (defined $row) {
+		template 'ok',
+		{
+        	'msg' => '502. Internal Error! <br />',
+		};
+    };
+    
+    $sql = 'INSERT INTO `login` (email, password, user_id) VALUES (?, ?, ?)';
+	$sth = $dbh->prepare($sql) or die $dbh->errstr;
+	$sth->execute($user_info->{email}, $user_info->{password}, $row->{id}) or die $dbh->errstr;
+
+    template 'ok',
+    {
+    	'msg' => 'User created successfully!<br />',
+    };
+
+};
 
 true;
